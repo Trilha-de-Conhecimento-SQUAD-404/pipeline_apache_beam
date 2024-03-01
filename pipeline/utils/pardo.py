e8dc73f6682d1342979f2360c22bb01031831561
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-01 17:42:04 -0300

    chore(pardo): write pardo class and process method

--- pipeline/utils/pardo.py
+++ pipeline/utils/pardo.py
@@ -3,10 +3,26 @@ import logging
 from apache_beam import DoFn
 from requests import get
 
-# generate file with pipeline information
+# Generate file with pipeline information
 logging.basicConfig(
     level=logging.INFO, 
     filename="json_to_csv.log", 
     format="%(asctime)s - %(levelname)s - %(message)s"
     )
 
+class PublicApis(DoFn): 
+    def process(self, element):    
+        url = "https://api.publicapis.org/entries"
+        response = get(url)
+        public_apis = response.json().get("entries")
+
+        try:                
+            logging.info(f"API retornou o status code: {response.status_code}")
+            
+            # If sucess, return separately each api json 
+            if response.status_code == 200: 
+                for api in public_apis:
+                    yield api
+                    
+        except Exception as e:
+            logging.error(f"Erro na requisição: {e}")
