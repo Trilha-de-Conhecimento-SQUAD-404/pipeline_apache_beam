05f0ef89ed19039197158d79b3a8ce72a7f46bd9
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-01 17:49:51 -0300

    chore(pipeline): import and call pardo class

--- pipeline/app.py
+++ pipeline/app.py
@@ -1,10 +1,11 @@
 import apache_beam as beam
+from utils.pardo import PublicApis 
 
 with beam.Pipeline() as pipeline:
     api_csv = (
         pipeline
         | "Trigger Pipeline" >> beam.Create([])
-        | "Api request" >> beam.ParDo()
+        | "Api request" >> beam.ParDo(PublicApis())
         | "JSON to csv" >> beam.Map()
         | "Write pages in a folder" >> beam.io.WriteToText("public_apis", 
                                                            header="API, Description, Auth, HTTPS, Cors, Link, Category")
